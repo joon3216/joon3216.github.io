762a6139d05f576cadef80530a853272d85c40f0
Junkyu Park <joon3216@gmail.com>
2019-06-03 19:27:03 -0400

    Deprecated cv.html and replaced with a pdf file. The order of items in navigation bar changed.

--- all_render.R
+++ all_render.R
@@ -13,60 +13,59 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 
 # Knit the entire .Rmd files in the repository
-# 
-# c(
-#     # 00_lvl, or the surface level
-#     'cv.Rmd',
-#     'index.Rmd',
-#     'news.Rmd',
-#     'projects.Rmd',
-#     'research_materials.Rmd',
-# 
-# 
-# 
-#     # 01_lvl
-#     ## news
-#     'news/2019-06.Rmd',
-# 
-#     ## projects
-#     'projects/2019.Rmd',
-# 
-#     ## research_materials
-#     'research_materials/2018.Rmd',
-#     'research_materials/2019.Rmd',
-# 
-#     ## style
-#     'style/testing.Rmd',
-#     'style/testing_architect.Rmd',
-#     'style/testing_hpstr.Rmd',
-#     'style/testing_tactile.Rmd',
-# 
-# 
-# 
-#     # 02_lvl
-#     ## projects
-#     ### 2019
-#     'projects/2019/publishing_website.Rmd',
-# 
-#     ## research_materials
-#     ### 2018
-#     'research_materials/2018/censored_data.Rmd',
-#     'research_materials/2018/non_separable_penalty.Rmd',
-#     'research_materials/2018/pgf.Rmd',
-#     'research_materials/2018/pgf_python.Rmd',
-#     'research_materials/2018/visualizing_confidence_regions.Rmd',
-# 
-#     ### 2019
-#     'research_materials/2019/cross_validation_fs.Rmd',
-#     'research_materials/2019/em_imputation.Rmd',
-#     'research_materials/2019/em_imputation_python.Rmd',
-#     'research_materials/2019/frog.Rmd',
-#     'research_materials/2019/largest_roll.Rmd',
-#     'research_materials/2019/matrix_derivatives.Rmd',
-#     'research_materials/2019/multivariate_response.Rmd'
-# ) %>%
-#     imap(function(x){render(x)}, .) %>%
-#     as.list()
+
+c(
+    # 00_lvl, or the surface level
+    'index.Rmd',
+    'news.Rmd',
+    'projects.Rmd',
+    'research_materials.Rmd',
+
+
+
+    # 01_lvl
+    ## news
+    'news/2019-06.Rmd',
+
+    ## projects
+    'projects/2019.Rmd',
+
+    ## research_materials
+    'research_materials/2018.Rmd',
+    'research_materials/2019.Rmd',
+
+    ## style
+    'style/testing.Rmd',
+    'style/testing_architect.Rmd',
+    'style/testing_hpstr.Rmd',
+    'style/testing_tactile.Rmd',
+
+
+
+    # 02_lvl
+    ## projects
+    ### 2019
+    'projects/2019/publishing_website.Rmd',
+
+    ## research_materials
+    ### 2018
+    'research_materials/2018/censored_data.Rmd',
+    'research_materials/2018/non_separable_penalty.Rmd',
+    'research_materials/2018/pgf.Rmd',
+    'research_materials/2018/pgf_python.Rmd',
+    'research_materials/2018/visualizing_confidence_regions.Rmd',
+
+    ### 2019
+    'research_materials/2019/cross_validation_fs.Rmd',
+    'research_materials/2019/em_imputation.Rmd',
+    'research_materials/2019/em_imputation_python.Rmd',
+    'research_materials/2019/frog.Rmd',
+    'research_materials/2019/largest_roll.Rmd',
+    'research_materials/2019/matrix_derivatives.Rmd',
+    'research_materials/2019/multivariate_response.Rmd'
+) %>%
+    imap(function(x){render(x)}, .) %>%
+    as.list()
 
 
 # Edit some .html files after rendering
