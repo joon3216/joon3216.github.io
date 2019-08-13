ff885f2d1f092d61158f3d4fab9aa04a49f4f699
Junkyu Park <joon3216@gmail.com>
2019-08-13 16:54:34 -0400

    index and projects page fixed

--- index_render.R
+++ index_render.R
@@ -2,7 +2,7 @@
 # Set a working directory to where this script is located
 # setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-# rmarkdown::render('index.Rmd')
+rmarkdown::render('index.Rmd')
 index <- paste(readLines('index.html', encoding = "UTF-8"), collapse = '\n')
 to_delete <- '<section class=\"page-header\">\n<h1 class=\"title toc-ignore project-name\">Junkyu Park</h1>\n</section>'
 index <- gsub(to_delete, '', index)
