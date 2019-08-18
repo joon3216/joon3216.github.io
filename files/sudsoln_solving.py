588ed1bcc624a0b59dc81438434cfaf82fb4f11f
Junkyu Park <joon3216@gmail.com>
2019-08-18 08:02:27 -0400

    result_sudsoln.csv designed to have category column

--- files/sudsoln_solving.py
+++ files/sudsoln_solving.py
@@ -10,25 +10,28 @@ top95 = np.loadtxt(
     dtype = str, 
     delimiter = '\n'
 )
+len_top95 = len(top95)
 hardest = np.loadtxt(
     'https://norvig.com/hardest.txt',
     dtype = str,
     delimiter = '\n'
 )
 concat = np.concatenate((top95, hardest))
+len_concat = len(concat)
 
 result_sudsoln = open('result_sudsoln.csv', 'w')
-result_sudsoln.write('result,time,trial,has_solved\n')
-for i in range(len(concat)):
+result_sudsoln.write('category,result,time,trial,is_solved\n')
+for i in range(len_concat):
     question = ss.to_sudoku(
         concat[i], 
         elements = set([j for j in range(1, 10)])
     )
     print('Question number', i + 1, '\n')
+    category = 'top95' if i <= len_top95 - 1 else 'hardest'
     time, trial = question.solve()
     result = "'" + str(question) + "'"
-    has_solved = question.is_valid_answer()
+    ans = question.is_valid_answer()
     result_sudsoln.write(
-        '{0},{1},{2},{3}\n'.format(result, time, trial, has_solved)
+        '{0},{1},{2},{3},{4}\n'.format(category, result, time, trial, ans)
     )
 result_sudsoln.close()
