8ac1402f0125471901af595bac268d2b89f2f2d4
Junkyu Park <joon3216@gmail.com>
2019-08-19 14:44:51 -0400

    Table 0 added

--- files/sudsoln_produce_results.py
+++ files/sudsoln_produce_results.py
@@ -3,18 +3,19 @@ import pandas as pd
 import sudsoln as ss
 
 path_to_result_sudsoln_csv = 'result_sudsoln' + ss.__version__ + '.csv'
-
+max_trial = 200
 
 # Produce a result
 
 def to_sec(time):
     h, m, s = time[0], time[2:4], time[5:]
     return float(h) * 3600 + float(m) * 60 + float(s)
+
 def categorize_solved(trial):
     if trial == 0:
         return 'logically'
     else:
-        if trial == 200:
+        if trial == max_trial:
             return 'not_solved'
         else:
             return 'forcefully'
@@ -22,6 +23,21 @@ def categorize_solved(trial):
 result_sudsoln = pd.read_csv(path_to_result_sudsoln_csv)
 result_sudsoln = result_sudsoln.iloc[:, [0, 2, 3, 4]]
 result_sudsoln.time = result_sudsoln.time.apply(to_sec)
+
+result_sudsoln_check = result_sudsoln\
+    .loc[lambda x: x['is_solved'] == False]
+if set(result_sudsoln_check.trial) == {max_trial}:
+    msg = 'Yes, is_solved = False iff trial == ' + str(max_trial)
+else:
+    msg = 'No, there is at least one case where' +\
+        ' is_solved == True and trial == ' + str(max_trial)
+print(
+    'Table 0: Check that is_solved = False iff trial == 200 ',
+    'before analysis:\n\n', 
+    result_sudsoln_check, '\n\n', msg,
+    sep = ''
+)
+
 result_sudsoln['min_time'] = result_sudsoln.time
 result_sudsoln['median_time'] = result_sudsoln.time
 result_sudsoln['avg_time'] = result_sudsoln.time
@@ -30,7 +46,10 @@ result_sudsoln.is_solved = result_sudsoln.is_solved.apply(int)
 result_sudsoln['total'] = 1
 result_sudsoln['solved'] = result_sudsoln.trial.apply(categorize_solved)
 
-result_sudsoln_report1 = result_sudsoln\
+result_sudsoln_cp = result_sudsoln.copy()
+result_sudsoln_cp.category = result_sudsoln_cp.category.apply(lambda x: 'all')
+result_sudsoln_dbl = result_sudsoln.append(result_sudsoln_cp)
+result_sudsoln_report1 = result_sudsoln_dbl\
     .groupby('category')\
     .agg({
         'is_solved': 'sum', 
@@ -48,8 +67,8 @@ print(
     sep = ''
 )
 
-result_sudsoln.is_solved = result_sudsoln.is_solved.apply(bool)
-result_sudsoln_report2 = result_sudsoln\
+result_sudsoln_dbl.is_solved = result_sudsoln_dbl.is_solved.apply(bool)
+result_sudsoln_report2 = result_sudsoln_dbl\
     .groupby(['category', 'is_solved'])\
     .agg({
         'total': 'sum',
