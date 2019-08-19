bd329d28e573dfb8438844fd1f9913fc1efa6c06
Junkyu Park <joon3216@gmail.com>
2019-08-19 15:10:02 -0400

    Added a newline char at the end of each table

--- files/sudsoln_produce_results.py
+++ files/sudsoln_produce_results.py
@@ -27,14 +27,15 @@ result_sudsoln.time = result_sudsoln.time.apply(to_sec)
 result_sudsoln_check = result_sudsoln\
     .loc[lambda x: x['is_solved'] == False]
 if set(result_sudsoln_check.trial) == {max_trial}:
-    msg = 'Yes, is_solved = False iff trial == ' + str(max_trial)
+    msg = '         Yes, is_solved = False iff trial == ' +\
+        str(max_trial) + ':'
 else:
-    msg = 'No, there is at least one case where' +\
-        ' is_solved == True and trial == ' + str(max_trial)
+    msg = '         No, there is at least one case where' +\
+        ' is_solved == True and trial == ' + str(max_trial) + ':'
 print(
     'Table 0: Check that is_solved = False iff trial == 200 ',
-    'before analysis:\n\n', 
-    result_sudsoln_check, '\n\n', msg,
+    'before analysis.\n' + msg + '\n\n', 
+    result_sudsoln_check, '\n',
     sep = ''
 )
 
@@ -47,7 +48,8 @@ result_sudsoln['total'] = 1
 result_sudsoln['solved'] = result_sudsoln.trial.apply(categorize_solved)
 
 result_sudsoln_cp = result_sudsoln.copy()
-result_sudsoln_cp.category = result_sudsoln_cp.category.apply(lambda x: 'all')
+result_sudsoln_cp.category =\
+    result_sudsoln_cp.category.apply(lambda x: 'all')
 result_sudsoln_dbl = result_sudsoln.append(result_sudsoln_cp)
 result_sudsoln_report1 = result_sudsoln_dbl\
     .groupby('category')\
@@ -62,8 +64,8 @@ result_sudsoln_report1 = result_sudsoln_dbl\
     .sort_values('category', ascending = False)
 print(
     'Table 1: Within each category, how many puzzles were solved?\n' +\
-    '         How fast were they solved?\n\n', 
-    result_sudsoln_report1, 
+    '         And how long did Sudoku.solve() run on average?\n\n', 
+    result_sudsoln_report1, '\n',
     sep = ''
 )
 
@@ -81,7 +83,7 @@ result_sudsoln_report2 = result_sudsoln_dbl\
 print(
     'Table 2: Within each category, if a puzzle was solved,\n' +\
     '         how long did it take to solve one?\n\n', 
-    result_sudsoln_report2, 
+    result_sudsoln_report2, '\n',
     sep = ''
 )
 
@@ -97,7 +99,7 @@ result_sudsoln_report3 = result_sudsoln\
     .reindex(['logically', 'forcefully', 'not_solved'])
 print(
     'Table 3: How many puzzles required a brute force to be solved?\n\n',
-    result_sudsoln_report3, 
+    result_sudsoln_report3, '\n',
     sep = ''
 )
 
@@ -118,6 +120,6 @@ result_sudsoln_report4 = result_sudsoln\
 print(
     'Table 4: If a brute force is used and successfully solved a puzzle, \n' +\
     '         how many attempts did it take to solve one?\n\n',
-    result_sudsoln_report4, 
+    result_sudsoln_report4, '\n',
     sep = ''
 )
